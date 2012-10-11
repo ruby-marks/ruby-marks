3da68fd9c8af688af2ce2ac22ff5d08988c27559
andrerpbts <andrerpbts@gmail.com>
2012-10-11 17:10:15 -0300

    The flood_scan method is part of document now

--- lib/ruby_marks/clock_mark.rb
+++ lib/ruby_marks/clock_mark.rb
@@ -15,17 +15,11 @@ module RubyMarks
 
     def calc_coordinates
       @coordinates.tap do |coordinates|
-        x1 = -1
-        x2 = -1
-        y1 = -1
-        y2 = -1
-
         if self.document
           x = position[:x]
           y = position[:y]
           
-          stack = flood_scan(x, y)
-          
+          stack = self.document.flood_scan(x, y)         
           x_elements = []
           y_elements = []
           stack.each do |k,v|
@@ -74,7 +68,6 @@ module RubyMarks
     end
 
 
-
     def valid?
       x_pos = coordinates[:x1]..coordinates[:x2]
       y_pos = coordinates[:y1]..coordinates[:y2]
@@ -116,90 +109,6 @@ module RubyMarks
 
     def to_s
       self.coordinates
-    end
-
-
-    private
-    def flood_scan(x, y)
-      result_mask =  Hash.new { |hash, key| hash[key] = [] }
-      result_mask.tap do |result_mask|
-        process_queue =  Hash.new { |hash, key| hash[key] = [] }
-        process_line = true
-        
-        loop do
-          reset_process = false
-
-          if process_line
-            current_x = x
-            loop do
-              color = self.document.file.pixel_color(current_x, y)
-              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)
-
-              break if !document.config.recognition_colors.include?(color) || current_x - 1 <= 0            
-              process_queue[y] << current_x unless process_queue[y].include?(current_x) || result_mask[y].include?(current_x) 
-              result_mask[y] << current_x unless result_mask[y].include?(current_x)            
-              current_x = current_x - 1
-            end
-
-            current_x = x
-            loop do
-              color = self.document.file.pixel_color(current_x, y)
-              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)
-
-              break if !document.config.recognition_colors.include?(color) || current_x + 1 >= self.document.file.page.width            
-              process_queue[y] << current_x unless process_queue[y].include?(current_x) || result_mask[y].include?(current_x)              
-              result_mask[y] << current_x unless result_mask[y].include?(current_x)
-              current_x = current_x + 1
-            end
-
-            result_mask[y] = result_mask[y].sort
-            process_queue[y] = process_queue[y].sort
-          end
-
-          process_line = true
-
-          process_queue[y].each do |element|
-            if y - 1 >= 0
-              color = self.document.file.pixel_color(element, y-1)
-              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)     
-              if document.config.recognition_colors.include?(color) && !result_mask[y-1].include?(element)
-                x = element
-                y = y - 1
-                reset_process = true
-                break
-              end
-            end
-          end
-
-          next if reset_process
-
-          process_queue[y].each do |element|         
-            if y + 1 <= self.document.file.page.height
-              color = self.document.file.pixel_color(element, y+1)
-              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)
-              if document.config.recognition_colors.include?(color) && !result_mask[y+1].include?(element)
-                x = element
-                y = y + 1
-                reset_process = true
-                break
-              else
-                process_queue[y].delete(element)
-              end
-            end
-          end
-
-          next if reset_process
-
-          process_queue.each do |k,v|
-            process_queue.delete(k) if v.empty?
-          end
-
-          break if process_queue.empty?
-
-          process_line = false
-          y = process_queue.first[0] if process_queue.first.is_a?(Array)
-        end
-      end
     end
 
   end
--- lib/ruby_marks/document.rb
+++ lib/ruby_marks/document.rb
@@ -155,6 +155,88 @@ module RubyMarks
       end
     end
 
+    def flood_scan(x, y)
+      result_mask =  Hash.new { |hash, key| hash[key] = [] }
+      result_mask.tap do |result_mask|
+        process_queue =  Hash.new { |hash, key| hash[key] = [] }
+        process_line = true
+        
+        loop do
+          reset_process = false
+
+          if process_line
+            current_x = x
+            loop do
+              color = self.file.pixel_color(current_x, y)
+              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)
+
+              break if !self.config.recognition_colors.include?(color) || current_x - 1 <= 0            
+              process_queue[y] << current_x unless process_queue[y].include?(current_x) || result_mask[y].include?(current_x) 
+              result_mask[y] << current_x unless result_mask[y].include?(current_x)            
+              current_x = current_x - 1
+            end
+
+            current_x = x
+            loop do
+              color = self.file.pixel_color(current_x, y)
+              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)
+
+              break if !self.config.recognition_colors.include?(color) || current_x + 1 >= self.file.page.width            
+              process_queue[y] << current_x unless process_queue[y].include?(current_x) || result_mask[y].include?(current_x)              
+              result_mask[y] << current_x unless result_mask[y].include?(current_x)
+              current_x = current_x + 1
+            end
+
+            result_mask[y] = result_mask[y].sort
+            process_queue[y] = process_queue[y].sort
+          end
+
+          process_line = true
+
+          process_queue[y].each do |element|
+            if y - 1 >= 0
+              color = self.file.pixel_color(element, y-1)
+              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)     
+              if self.config.recognition_colors.include?(color) && !result_mask[y-1].include?(element)
+                x = element
+                y = y - 1
+                reset_process = true
+                break
+              end
+            end
+          end
+
+          next if reset_process
+
+          process_queue[y].each do |element|         
+            if y + 1 <= self.file.page.height
+              color = self.file.pixel_color(element, y+1)
+              color = RubyMarks::RGB.to_hex(color.red, color.green, color.blue)
+              if self.config.recognition_colors.include?(color) && !result_mask[y+1].include?(element)
+                x = element
+                y = y + 1
+                reset_process = true
+                break
+              else
+                process_queue[y].delete(element)
+              end
+            end
+          end
+
+          next if reset_process
+
+          process_queue.each do |k,v|
+            process_queue.delete(k) if v.empty?
+          end
+
+          break if process_queue.empty?
+
+          process_line = false
+          y = process_queue.first[0] if process_queue.first.is_a?(Array)
+        end
+      end
+    end
+
     private
     def add_mark(file)
       @gc.annotate(file, 0, 0, current_position[:x]-9, current_position[:y]+11, "+") do
@@ -170,6 +252,7 @@ module RubyMarks
       dr.point(current_position[:x] + 1, current_position[:y] + 1)             
       dr.draw(file)
     end
+    
 
   end
 
